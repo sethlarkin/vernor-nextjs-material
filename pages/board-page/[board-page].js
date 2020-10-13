f40c78b66c3088767fc6db4c90327d918a5743f3
sethlarkin <sethlarkin@gmail.com>
2020-10-13 13:01:39 -0700

    comment out vdeio for now

--- pages/board-page/[board-page].js
+++ pages/board-page/[board-page].js
@@ -77,16 +77,16 @@ export default function BoardPage({ props, boardData }) {
                         <h6>Volume: {boardDetail.dims.Volume}</h6>
                     </div>
                     <br />
-                    <div>
-                    <iframe 
-                        width="560" 
-                        height="315" 
-                        src="https://www.youtube.com/embed/fPmplk0aAA8" 
-                        frameBorder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                        allowFullScreen>
-                    </iframe>
-                    </div>
+                    {/* <div>
+                        <iframe
+                            width="560"
+                            height="315"
+                            src="https://www.youtube.com/embed/fPmplk0aAA8"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen>
+                        </iframe>
+                    </div> */}
                 </div>
             </div>
             <style jsx>{`
